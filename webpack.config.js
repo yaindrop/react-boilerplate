430bb94f21211e5fb546aa573bd1f7db853298bb
diff3usion <diff3usion@outlook.com>
2021-12-16 13:09:23 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const APP_PATH = path.resolve(__dirname, 'src');
+
+module.exports = {
+    entry: APP_PATH,
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    devServer: {
+        open: true,
+    },
+    performance: {
+        hints: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ inject: true, template: path.join(APP_PATH, 'index.html') }),
+        new ForkTsCheckerWebpackPlugin(),
+    ],
+    module: {
+        rules: [{
+            test: /\.(ts|js)x?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        },
+        {
+            test: /\.(css|scss)$/,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+            use: ["file-loader"],
+        }],
+    },
+};
